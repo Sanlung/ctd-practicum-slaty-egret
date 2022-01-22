ecc68edefa1018f2218d628305aba8f45af9b486
Asel-K <asel.karagazieva@gmail.com>
2022-01-22 01:31:35 -0600

    adding logo component

---
+++ components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <img src="/Logo.png" alt="Logo"></img>
+        </div>
+    )
+}
+
+export default Logo; 
