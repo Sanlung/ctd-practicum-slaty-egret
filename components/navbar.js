4da2d36ee69befc527501bb71121b6e1324e196c
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2022-01-24 15:51:53 -0800

    added a Navbar component with a Login button

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <button type="submit">Login</button>
+    );
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import {siteTitle} from "../components/layout";
+import { siteTitle } from "../components/layout";
 import App from "../components/App";
+import Navbar from "../components/Navbar"
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
           <span className='logo-w'>W</span>
           <span className='logo'>hatodo App</span>{" "}
         </h1>
+        <Navbar />
         <p>Welcome aboard. Let&apos;s get our hands dirty!</p>
         <App />
       </div>
