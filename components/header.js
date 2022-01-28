26aa543f93296a555600708b3f02b83964bf50aa
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2022-01-27 17:01:52 -0800

    initializing FirebaseUI

--- components/header.js
+++ components/header.js
@@ -1,3 +1,8 @@
-const Header = () => <header>Here is the header</header>;
+const Header = () => {
 
+    <header>
+        <script src="https://www.gstatic.com/firebasejs/ui/6.0.0/firebase-ui-auth.js"></script>
+        <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.0.0/firebase-ui-auth.css" />
+    </header>;
+};
 export default Header;
--- components/navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar = () => {
-    return (
-        <button type="submit">Login</button>
-    );
-}
-
-export default Navbar;
