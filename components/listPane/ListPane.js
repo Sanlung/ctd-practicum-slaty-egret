da2c2ad9b1c993c86ffb1d62fe7f882f13c00ab4
Asel-K <asel.karagazieva@gmail.com>
2022-01-31 16:09:27 -0600

    updated listpane with local stotage

--- components/listPane/ListPane.js
+++ components/listPane/ListPane.js
@@ -1,11 +1,32 @@
 import TodoList from "./TodoList";
-import AddNewEntryForm from "../AddNewEntryForm";
+// import AddNewEntryForm from "./AddNewEntryForm";
+import React, { useEffect, useState } from "react";
 
-const ListPane = () => (
+function useSemiPersistentState () {
+    const [list, setList] = useState (JSON.parse(localStorage.getItem('savedList')));
+    useEffect(() => {
+        localStorage.setItem('savedList', JSON.stringify(list));
+    }, [list]);
+    return(
+        [list, setList]
+    )
+};
+
+const ListPane = () => {
+
+    const [list, setList] = useSemiPersistentState();
+    
+    const addItem = (newItem) => {
+        setList([...list, newItem])
+    }
+    return(
+
   <>
-    <TodoList />
-    <AddNewEntryForm />
+
+    <TodoList list={list}/>
+    {/* <AddNewEntryForm onAddItem={addItem}  /> */}
   </>
-);
+    )
+};
 
 export default ListPane;
--- pages/lists/[id].js
+++ pages/lists/[id].js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout, {siteTitle} from "../../components/Layout";
+import ListPane from "../../components/listPane/ListPane";
 
 const List = () => (
   <Layout>
@@ -8,6 +9,7 @@ const List = () => (
     </Head>
     <div>
       <h1>Write the todo list page here using ListPane component</h1>
+      <ListPane />
     </div>
   </Layout>
 );
