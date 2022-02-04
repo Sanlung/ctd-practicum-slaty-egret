18e39eecee425b17637e2748a377dbf504fd5810
Asel-K <asel.karagazieva@gmail.com>
2022-02-04 00:33:32 -0600

    updated logo component using next.js

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -1,3 +1,16 @@
-const Logo = () => <div>Site logo here</div>;
+import Image from 'next/image'
+
+const Logo = () => {
+    return (
+        <>
+            <Image 
+            src="/../public/images/Whatodogrey.svg"
+            alt="Logos"
+            width="150"
+            height="150"
+            />
+        </>
+    )
+}
 
 export default Logo;
