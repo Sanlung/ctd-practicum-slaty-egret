8d394febe0ab438e941246e7dc41730ca5806c1f
Asel-K <asel.karagazieva@gmail.com>
2022-02-04 15:21:10 -0600

    updated src and removed widthheight

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -1,13 +1,12 @@
 import Image from 'next/image'
+import Whatodogrey from '../../public/images/Whatodogrey.svg'
 
 const Logo = () => {
     return (
         <>
             <Image 
-            src="/../public/images/Whatodogrey.svg"
-            alt="Logos"
-            width="150"
-            height="150"
+            src={Whatodogrey}
+            alt="Logos" 
             />
         </>
     )
