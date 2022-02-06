1e533871f07f9d41b7e32448a2e69b71621d263e
Chung Kao <chungckao@gmail.com>
2022-02-06 14:03:39 +0800

    Modify dimensions of logo svg files for better import and rendering

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -1,15 +1,12 @@
-import Image from 'next/image'
-import Whatodogrey from '../../public/images/Whatodogrey.svg'
+import Image from "next/image";
+import Whatodogrey from "../../public/images/Whatodogrey.svg";
 
 const Logo = () => {
-    return (
-        <>
-            <Image 
-            src={Whatodogrey}
-            alt="Logos" 
-            />
-        </>
-    )
-}
+  return (
+    <>
+      <Image src={Whatodogrey} alt='Logos' height='50' width='150' />
+    </>
+  );
+};
 
 export default Logo;
