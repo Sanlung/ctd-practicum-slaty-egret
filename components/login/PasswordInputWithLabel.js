2f6f1c0bdd498707d37c7f6d2616fb8b1e558f27
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2022-02-16 16:40:52 -0800

    Refactores index.js user authentication into reusable functional components

--- components/login/PasswordInputWithLabel.js
+++ components/login/PasswordInputWithLabel.js
@@ -1,9 +1,21 @@
-const PasswordInputWithLabel = () => (
-    <div>
-        <input type="password" id="txtPassword" value={password}
-            onChange={({ target }) => setPassword(target.value)} />
-        <label>Password</label>
-    </div>
-);
+const PasswordInputWithLabel = (props) => {
+
+    const handlePassword = (event) => {
+        props.onChange(event.target.value);
+    }
+
+    return (
+        <div>
+            <input
+                type="password"
+                value={props.value}
+                placeholder={props.labelTextContent}
+                // onfocus="this.placeholder=''"
+                // onblur="this.placeholder={props.labelTextContent}"
+                onChange={handlePassword} />
+            <label className="visuallyhidden">{props.labelTextContent}</label>
+        </div>
+    );
+};
 
 export default PasswordInputWithLabel;
--- pages/index.js
+++ pages/index.js
@@ -1,94 +1,12 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useState } from 'react';
 import { siteTitle } from "../components/Layout";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "firebase/auth"
-import { firebaseApp } from "../config/firebaseConfig";
-
-
+import LoginForm from "../components/login/LoginForm";
+import SignupForm from "../components/login/SignupForm";
 
 const Home = () => {
 
   const router = useRouter();
-  const auth = getAuth(firebaseApp);
-
-  const [loginEmail, setLoginEmail] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
-  const [signupEmail, setSignupEmail] = useState('');
-  const [signupPassword, setSignupPassword] = useState('');
-  const [signupPassConf, setSignupPassConf] = useState('');
-  const [loginNotification, setLoginNotification] = useState('');
-  const [signupNotification, setSignupNotification] = useState('');
-  const [logoutNotification, setLogoutNotification] = useState('');
-
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-      .then(
-        setLoginNotification(
-          'You are logged in'
-        ),
-        setTimeout(() => {
-          setLoginNotification('')
-        }, 2000)
-      )
-      .catch((err) => {
-        console.log(err.code, err.message)
-        setLoginNotification(err.message)
-        setTimeout(() => {
-          setLoginNotification('')
-        }, 2000)
-      })
-    setLoginEmail('')
-    setLoginPassword('')
-  }
-
-  const handleSignup = (e) => {
-    e.preventDefault();
-    if (signupPassword !== signupPassConf) {
-      setSignupNotification(
-        'Password and password confirmation does not match'
-      )
-      setTimeout(() => {
-        setSignupNotification('')
-      }, 2000)
-      setSignupPassword('');
-      setSignupPassConf('');
-      return null;
-    }
-    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
-      .catch((err) => {
-        console.log(err.code, err.message)
-      });
-  }
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-    signOut(auth)
-      .then(() => {
-        setLogoutNotification('You are logged out')
-        setTimeout(() => {
-          setLogoutNotification('')
-        }, 2000),
-          console.log('User is logged out')
-      });
-  }
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log(uid);
-    } else {
-      console.log('User is signed out');
-    }
-  });
 
 
   return (
@@ -99,41 +17,11 @@ const Home = () => {
       </Head>
 
       <div>
-        <h1>Login</h1>
-        {loginNotification}
-        <form onSubmit={handleLogin}>
-          Email: <input type="text" value={loginEmail}
-            onChange={({ target }) => setLoginEmail(target.value)} />
-          <br />
-          Password: <input type="password" value={loginPassword}
-            onChange={({ target }) => setLoginPassword(target.value)} />
-          <br />
-          <button type="submit">Login</button>
-        </form>
+        <LoginForm />
       </div>
 
-      <br />
-
       <div>
-        <h1>Sign up</h1>
-        {signupNotification}
-        <form onSubmit={handleSignup}>
-          Email: <input type="text" value={signupEmail}
-            onChange={({ target }) => setSignupEmail(target.value)} />
-          <br />
-          Password: <input type="password" value={signupPassword}
-            onChange={({ target }) => setSignupPassword(target.value)} />
-          <br />
-          Confirm password: <input type="password" value={signupPassConf}
-            onChange={({ target }) => setSignupPassConf(target.value)} />
-          <br />
-          <button type="submit">Sign up</button>
-        </form>
-      </div>
-      <br />
-      {logoutNotification}
-      <div>
-        <button type='button' onClick={handleLogout}>Logout</button>
+        <SignupForm />
       </div>
 
     </>
