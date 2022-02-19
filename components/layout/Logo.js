51feef6640028933ee3b75ddb44f1110c911c909
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2022-02-18 17:29:57 -0800

    Fix merge conflict

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
-import Image from 'next/image'
-import Whatodogrey from '../../public/images/Whatodogrey.svg'
-=======
 import Image from "next/image";
 import Whatodogrey from "../../public/images/Whatodogrey.svg";
 
 const Logo = () => {
-  return (
-    <>
-      <Image src={Whatodogrey} alt='Logos' height='50' width='150' />
-    </>
-  );
+    return (
+        <>
+            <Image src={Whatodogrey} alt='Logos' height='50' width='150' />
+        </>
+    );
 };
->>>>>>> main
 
 const Logo = () => {
     return (
--- components/layout/LogoutButton.js
+++ components/layout/LogoutButton.js
@@ -1,15 +1,7 @@
 import Link from "next/link";
-<<<<<<< HEAD
 import {
   getAuth,
   signOut
-=======
-import { useState } from "react";
-import {
-  getAuth,
-  signOut,
-  onAuthStateChanged
->>>>>>> 4468c71cefbad51b712877fdf58b455237f2b1b5
 } from "firebase/auth"
 import { firebaseApp } from "../../config/firebaseConfig";
 
@@ -17,34 +9,12 @@ import { firebaseApp } from "../../config/firebaseConfig";
 const LogoutButton = () => {
 
   const auth = getAuth(firebaseApp);
-<<<<<<< HEAD
 
   const handleLogout = (e) => {
     e.preventDefault();
     signOut(auth);
   };
 
-=======
-  const [logoutNotification, setLogoutNotification] = useState('');
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-    signOut(auth)
-      .then(() => {
-        console.log('User is logged out')
-      });
-  };
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log(uid);
-    } else {
-      console.log('User is signed out');
-    }
-  });
-
->>>>>>> 4468c71cefbad51b712877fdf58b455237f2b1b5
   return (
     <>
       <Link href='/'>
