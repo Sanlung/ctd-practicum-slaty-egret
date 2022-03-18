9288595d8ddbc38ee290fd48ca3ec3c413eb9ed2
Chung Kao <chungckao@gmail.com>
2022-03-18 17:45:36 +0800

    Bkgd video fix as old one didn't work for > 24" screen, and css bug fix

--- components/layout/Background.js
+++ components/layout/Background.js
@@ -1,11 +1,11 @@
 const Background = () => (
-  <div>
+  <>
     <video autoPlay muted loop className='bgvideo'>
-      <source src='/images/waves.mp4' type='video/mp4' />
-      <source src='/images/waves.webm' type='video/webm' />
+      <source src='/images/fluid.mp4' type='video/mp4' />
+      <source src='/images/fluid.webm' type='video/webm' />
       Your browser is not supported!
     </video>
-  </div>
+  </>
 );
 
 export default Background;
